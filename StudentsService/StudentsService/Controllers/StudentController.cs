f4cea2e7ef1a2c2bb98abe844863db1279865326
satsargsyan <satenik.sargsyan2017@gmail.com>
2017-05-22 14:57:57 +0400

    With Entity

---
+++ StudentsService/StudentsService/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentsDataAccess;
+
+namespace StudentsService.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public IEnumerable<Student> Get()
+        {
+            using (BetSatSargsyanEntities entities = new BetSatSargsyanEntities())
+            {
+                return entities.Students.ToList();
+            }
+             
+        }
+
+               public Student Get(int id)
+        {
+            using (BetSatSargsyanEntities entities = new BetSatSargsyanEntities())
+            {
+                return entities.Students.FirstOrDefault(e=>e.StudentID==id);
+            }
+        }
+    }
+}
